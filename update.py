0dbc1b9d86a7bfd468a89ad13417acda5edc7286
7tick <75943417+7tick@users.noreply.github.com>
2022-04-01 17:34:15 +0100

    Update and rename rickroll.py to update.py

--- update.py
+++ update.py
@@ -2,11 +2,10 @@
 import time
 import webbrowser
 import random
-
 i = 0
 while i < 3:
     randomTime = random.randint(10, 20)
-    print ("Prank will execute in "+str(randomTime)+" minutes!")
+    print (str(randomTime))
     time.sleep(randomTime * 60)
     webbrowser.open("https://www.tomorrowtides.com/rickrolled85.html")
     i = i + 1
